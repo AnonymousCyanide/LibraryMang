842b0c7b536e3704747717bfb0a288ee88575c13
Avim <avidubey2000@gmail.com>
2022-07-16 02:30:23 +0530

    Added get_Data and post_book in api

--- django-interview-assignment-master/library/api/views.py
+++ django-interview-assignment-master/library/api/views.py
@@ -1,11 +1,17 @@
+import imp
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from Books.models import Book
+from api.serializers import BookSerializer
 @api_view(['GET'])
 def get_Data(request):
-    book = {
-        'id' : 'xyz',
-        'title': 'abc',
-        'status' : 'Borrowed',
-        'Return_Date' : 'yyyy-mm-dd'
-    }
-    return Response(book)
+    books = Book.objects.all()
+    ser = BookSerializer(books,many = True)
+    return Response(ser.data)
+@api_view(['POST'])
+def add_book(request):
+    ser = BookSerializer(data = request.data)
+    if ser.is_valid():
+        ser.save()
+        return Response(ser.data , 200)
+    return Response(403)
--- django-interview-assignment-master/library/library/settings.py
+++ django-interview-assignment-master/library/library/settings.py
@@ -34,10 +34,13 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # INSTALLED APPs
     'rest_framework',
+    # My Apps
+    'Books',
 ]
 
 MIDDLEWARE = [
