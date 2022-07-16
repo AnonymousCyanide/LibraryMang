0ae1085dd23ab49ed75df6a2d086420562c00547
Avim <avidubey2000@gmail.com>
2022-07-17 00:26:36 +0530

    Removed borrowed_till and status from Book model added price

--- django-interview-assignment-master/library/Books/models.py
+++ django-interview-assignment-master/library/Books/models.py
@@ -3,8 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
-    status_choices = (('Borrowed','Borrowed'),('Available','Available'))
     title = models.CharField(max_length= 40 ,default='Unknown')
-    status = models.CharField(max_length=10, choices= status_choices , default='Available')
-    borrowed_till = models.DateField(null=True , blank=True)
+    price = models.FloatField(default= 5)
+   
     
