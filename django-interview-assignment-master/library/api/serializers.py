17592505197def859db37ae62e07929b6cd55bad
Avim <avidubey2000@gmail.com>
2022-07-17 23:52:42 +0530

    Added Borrowed serializer

--- django-interview-assignment-master/library/api/serializers.py
+++ django-interview-assignment-master/library/api/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
-from Books.models import Book
+from Books.models import *
+
 class BookSerializer(serializers.ModelSerializer):
     class Meta :
         model = Book
+        fields = '__all__'
+
+class BorrowedSerializer(serializers.ModelSerializer):
+    class Meta :
+        model = Borrowed
         fields = '__all__'
