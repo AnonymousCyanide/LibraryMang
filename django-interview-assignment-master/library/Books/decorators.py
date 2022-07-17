8783937a63102248c328e8e60f52614002a1ca2f
Avim <avidubey2000@gmail.com>
2022-07-17 23:50:35 +0530

    Modified decorators to support multiple groups

--- django-interview-assignment-master/library/Books/decorators.py
+++ django-interview-assignment-master/library/Books/decorators.py
@@ -1,20 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
+def check_grp(names,roles):
+    for name in names:
+        if name in roles:
+            return True
+    return False
+
 def allowed_users(allowed_roles= []):
     def decorator(view_func):
         def wrapper(request , *args, **kwargs):
             print(allowed_roles)
             group = None
             if request.user.groups.exists():
-                group = request.user.groups.all()[0].name
-                if group in allowed_roles:
+                groups = list(request.user.groups.all())
+                names = []
+                for group in groups:
+                    names.append(group.name)
+                print(names)
+                if check_grp(names , allowed_roles):
                     return view_func(request , *args, **kwargs)
                 else :
                     print('I am here')
-                    return redirect('user_page')
+                    return redirect('home_page')
             else :
-                return redirect('user_page')
+                return redirect('home_page')
     
         return wrapper
     return decorator
