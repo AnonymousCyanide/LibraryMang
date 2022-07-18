9f104801f87e747492e4f1c4d3f7fc189838f946
Avim <avidubey2000@gmail.com>
2022-07-19 03:29:21 +0530

    get_book_data authenticated by permission_class decorator

--- django-interview-assignment-master/library/api/views.py
+++ django-interview-assignment-master/library/api/views.py
@@ -1,14 +1,16 @@
-import imp
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view , permission_classes
 from Books.models import Book
 from api.serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_Data(request):
     books = Book.objects.all()
     ser = BookSerializer(books,many = True)
     return Response(ser.data)
+
 @api_view(['POST'])
 def add_book(request):
     ser = BookSerializer(data = request.data)
@@ -16,3 +18,4 @@ def add_book(request):
         ser.save()
         return Response(ser.data , 200)
     return Response(403)
+
